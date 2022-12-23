3997c3bffd0ffeed4a614d39ab893fc969bb2271
HiroIshida <spitfire.docomo@gmail.com>
2022-12-23 10:56:47 +0900

    feat: make ert working

--- action.sh
+++ action.sh
@@ -1,3 +1,3 @@
 cd build
-cmake ..
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug ..
 make -j4
--- example.cpp
+++ example.cpp
@@ -3,12 +3,15 @@
 #include <ompl/base/SpaceInformation.h>
 #include <ompl/geometric/SimpleSetup.h>
 #include <ompl/geometric/planners/rrt/RRTConnect.h>
+#include <ompl/tools/lightning/LightningDB.h>
 #include <experience/ERT.h>
 
 #include <memory>
 
 namespace ob = ompl::base;
 namespace og = ompl::geometric;
+namespace ot = ompl::tools;
+
 
 int main(){
   // create planner for 2d square space
@@ -23,6 +26,8 @@ int main(){
   // create many example data
   std::vector<std::vector<ob::State*>> experiences;
 
+  auto database = ot::LightningDB(space);
+
   for(size_t i = 0; i < 200; ++i){
     const auto valid_sampler = setup_rrt->getSpaceInformation()->allocValidStateSampler();
     ob::ScopedState<> start(setup_rrt->getStateSpace());
@@ -33,7 +38,9 @@ int main(){
     const auto result = setup_rrt->solve(1.0);
     const bool solved = result && result != ob::PlannerStatus::APPROXIMATE_SOLUTION;
     if(solved){
-      const auto p = setup_rrt->getSolutionPath().as<og::PathGeometric>();
+      auto p = setup_rrt->getSolutionPath().as<og::PathGeometric>();
+      double insertion_time;
+      database.addPath(*p, insertion_time);
       const auto experience = p->getStates();
       experiences.push_back(experience);
     }
@@ -43,15 +50,28 @@ int main(){
   const auto ert_setup = std::make_unique<og::SimpleSetup>(si);
   ert_setup->setStateValidityChecker([](const ob::State* s) { return true; });
   const auto ert_planner = std::make_shared<og::ERT>(si);
-  for(const auto& experience : experiences){
-    ert_planner->setExperience(experience);
-  }
-  ert_setup->setPlanner(ert_planner);
 
   const auto valid_sampler = ert_setup->getSpaceInformation()->allocValidStateSampler();
   ob::ScopedState<> start(ert_setup->getStateSpace());
   valid_sampler->sample(start.get());
   ob::ScopedState<> goal(ert_setup->getStateSpace());
   valid_sampler->sample(goal.get());
+  auto start_ = start->as<ob::State>();
+  auto goal_ = goal->as<ob::State>();
+  const auto selected_path = database.findNearestStartGoal(1, start_, goal_).at(0);
+
+  const auto chosenPath = database.findNearestStartGoal(1, start_, goal_).at(0);
+
+  assert(chosenPath->numVertices() >= 2);
+
+  auto chosen_path_geometric(std::make_shared<og::PathGeometric>(si));
+  for (std::size_t i = 0; i < chosenPath->numVertices(); ++i)
+  {
+      chosen_path_geometric->append(chosenPath->getVertex(i).getState());
+  }
+  const auto experience = chosen_path_geometric->getStates();
+  ert_planner->setExperience(experience);
+  ert_setup->setPlanner(ert_planner);
+  ert_setup->setStartAndGoalStates(start, goal);
   ert_setup->solve(1.0);
 }
